4309dd2a61f78a6001059f23e60d26214e992615
Jinghao xu <jinghaoxu@jinghaodembp.mynetworksettings.com>
2024-07-06 00:41:11 -0400

    Updates

---
+++ precache-manifest.78b55cf1bf403c9676a2b9df5fe764dc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9588aedc6796a6b001fe64433d63bc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "677c382cc08e5d265db5",
+    "url": "/my-app/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "e3d97516a6f35955d620",
+    "url": "/my-app/static/css/main.14819343.chunk.css"
+  },
+  {
+    "revision": "677c382cc08e5d265db5",
+    "url": "/my-app/static/js/2.c41947ca.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/my-app/static/js/2.c41947ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3d97516a6f35955d620",
+    "url": "/my-app/static/js/main.e7497061.chunk.js"
+  },
+  {
+    "revision": "16c42100ba74878e3357",
+    "url": "/my-app/static/js/runtime-main.97ffa0d1.js"
+  },
+  {
+    "revision": "107bd96380db95b7086c57ed7a17a268",
+    "url": "/my-app/static/media/Gomoku.107bd963.png"
+  },
+  {
+    "revision": "cc964529897adbe9b057b46848c75a71",
+    "url": "/my-app/static/media/Robin.cc964529.png"
+  },
+  {
+    "revision": "180770c4e1d6782f9e68d29300b1a093",
+    "url": "/my-app/static/media/drone.180770c4.jpg"
+  },
+  {
+    "revision": "915b44109ad44ba87b4c6344486fd8b1",
+    "url": "/my-app/static/media/dsp.915b4410.png"
+  },
+  {
+    "revision": "9f050fe8942069a6a4155312b16778af",
+    "url": "/my-app/static/media/flyback.9f050fe8.png"
+  },
+  {
+    "revision": "67bded3f7fdd4a482af56b590fbf167c",
+    "url": "/my-app/static/media/opti.67bded3f.png"
+  },
+  {
+    "revision": "21b4e3f3f9477b71532dc994412d210d",
+    "url": "/my-app/static/media/pic.21b4e3f3.jpg"
+  }
+]);
